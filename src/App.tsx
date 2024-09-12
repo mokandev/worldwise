8dcfb4f11c690157b195371d8ddec4e4fd2c50ee
Carlos <carlos.flach15@gmail.com>
2024-09-12 15:38:55 -0300

    feat: working with nested routes and index route

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,16 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing/>}/>
         <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />} >
+          <Route index element={ <p> List of Cities Here</p>} />
+          <Route path="cities" element={ <p> List of Cities Here</p>} />
+          <Route path="countries" element={<p>List of Contries here</p>} />
+          <Route path="form" element={<p> Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- src/components/AppNav.tsx
+++ src/components/AppNav.tsx
@@ -1,7 +1,16 @@
-
-import styles from "./AppNav.module.css"
+import { NavLink } from "react-router-dom";
+import styles from "./AppNav.module.css";
 export default function AppNav() {
   return (
-    <nav className={styles.nav}>AppNav</nav>
-  )
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to="cities">Cities</NavLink>{" "}
+        </li>
+        <li>
+          <NavLink to="countries">Contries</NavLink>{" "}
+        </li>
+      </ul>
+    </nav>
+  );
 }
