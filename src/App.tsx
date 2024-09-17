345575baf2f33cde1b3d73fe6c555acd8969bf69
Carlos Afonso <carlos.flach15@gmail.com>
2024-09-17 20:50:19 -0300

    feat: reading an setting a query string

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,67 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AppLayout from "./pages/AppLayout";
-import Homepage from "./pages/Homepage";
-import Product from "./pages/Product";
-import Pricing from "./pages/Pricing";
-import PageNotFound from "./pages/PageNotFound";
-import Login from "./pages/Login";
-import CityList from "./components/CityList";
-import { useEffect, useState } from "react";
-import CountryList from "./components/CountryList";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AppLayout from './pages/AppLayout';
+import Homepage from './pages/Homepage';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import PageNotFound from './pages/PageNotFound';
+import Login from './pages/Login';
+import CityList from './components/CityList';
+import { useEffect, useState } from 'react';
+import CountryList from './components/CountryList';
+import City from './components/City';
 
-const BASE_URL = "http://localhost:8000"
+const BASE_URL = 'http://localhost:8000';
 
 export interface ICity {
-  cityName: string
-  country: string
-  emoji: string
-  date: string
-  notes: string
-  position: {
-    lat: number,
-    lng: number
-  },
-  id: number
+	cityName: string;
+	country: string;
+	emoji: string;
+	date: string;
+	notes: string;
+	position: {
+		lat: number;
+		lng: number;
+	};
+	id: number;
 }
 
 export default function App() {
+	const [cities, setCities] = useState<ICity[]>([]);
+	const [isLoading, setIsLoading] = useState(false);
 
-  const [cities, setCities] = useState<ICity[]>([]);
-  const [isLoading, setIsLoading] = useState(false)
+	async function fetchCities() {
+		try {
+			setIsLoading(true);
+			const res = await fetch(`${BASE_URL}/cities`);
+			const data = await res.json();
+			setCities(data);
+		} catch {
+			alert('There was an error loading data..');
+		} finally {
+			setIsLoading(false);
+		}
+	}
 
-  async function fetchCities() {
-    try {
-      setIsLoading(true)
-      const res = await fetch(`${BASE_URL}/cities`)
-      const data = await res.json()
-      setCities(data)
+	useEffect(() => {
+		fetchCities();
+	}, []);
 
-    } catch {
-      alert("There was an error loading data..")
-    } finally {
-      setIsLoading(false)
-    }
-  }
-
-  useEffect(() => {
-    fetchCities()
-  },[])
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Homepage />} />
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing/>}/>
-        <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} >
-          <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
-          <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
-          <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading}/>} />
-          <Route path="form" element={<p> Form</p>} />
-        </Route>
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
-  )
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route index element={<Homepage />} />
+				<Route path='product' element={<Product />} />
+				<Route path='pricing' element={<Pricing />} />
+				<Route path='login' element={<Login />} />
+				<Route path='app' element={<AppLayout />}>
+					<Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+					<Route path='cities' element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route path="cities/:id" element={<City />} />
+					<Route path='countries' element={<CountryList cities={cities} isLoading={isLoading} />} />
+					<Route path='form' element={<p> Form</p>} />
+				</Route>
+				<Route path='*' element={<PageNotFound />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
--- src/components/CityItem.tsx
+++ src/components/CityItem.tsx
@@ -1,25 +1,30 @@
-import { ICity } from "../App";
-import styles from "./CityItem.module.css";
+import { Link } from 'react-router-dom';
+import { ICity } from '../App';
+import styles from './CityItem.module.css';
 
 interface ICityItemProps {
-  city: ICity;
+	city: ICity;
 }
 
 const formatDate = (date: string) =>
-    new Intl.DateTimeFormat("en", {
-      day: "numeric",
-      month: "long",
-      year: "numeric",
-    }).format(new Date(date));
+	new Intl.DateTimeFormat('en', {
+		day: 'numeric',
+		month: 'long',
+		year: 'numeric',
+	}).format(new Date(date));
 
 export default function CityItem({ city }: ICityItemProps) {
-  const { cityName, emoji, date } = city;
-  return (
-    <li className={styles.cityItem}>
-      <span className={styles.emoji}>{emoji}</span>
-      <h3 className={styles.name}>{cityName}</h3>
-      <time className={styles.date}>{formatDate(date)}</time>
-      <button className={styles.deleteBtn}>&times; </button>
-    </li>
-  );
+	const { cityName, emoji, date, id, position } = city;
+
+
+	return (
+		<li>
+			<Link to={`${id}?lat=${position.lat}&lng=${position.lng}`} className={styles.cityItem}>
+				<span className={styles.emoji}>{emoji}</span>
+				<h3 className={styles.name}>{cityName}</h3>
+				<time className={styles.date}>{formatDate(date)}</time>
+				<button className={styles.deleteBtn}>&times; </button>
+			</Link>
+		</li>
+	);
 }
