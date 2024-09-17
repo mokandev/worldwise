e7075d6f199637bef53f30081774b6f02f2f4368
Carlos <carlos.flach15@gmail.com>
2024-09-17 16:05:41 -0300

    feat: implementing the countries list

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import { useEffect, useState } from "react";
+import CountryList from "./components/CountryList";
 
 const BASE_URL = "http://localhost:8000"
 
@@ -20,7 +21,7 @@ export interface ICity {
     lat: number,
     lng: number
   },
-  id: string
+  id: number
 }
 
 export default function App() {
@@ -56,7 +57,7 @@ export default function App() {
         <Route path="app" element={<AppLayout />} >
           <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
           <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
-          <Route path="countries" element={<p>List of Contries here</p>} />
+          <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading}/>} />
           <Route path="form" element={<p> Form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
---
+++ src/components/CountryList.tsx
@@ -0,0 +1,52 @@
+import { ICity } from "../App";
+import CountryItem from "./CountryItem";
+import styles from "./CountryList.module.css";
+import Message from "./Message";
+import Spinner from "./Spinner";
+
+interface ICityListProps {
+  isLoading: Boolean;
+  cities: ICity[];
+}
+
+interface ICountry {
+  country: string
+  emoji: string
+  id: number
+}
+
+export default function CountryList({ cities, isLoading }: ICityListProps) {
+
+  console.log('cities', cities)
+  if (isLoading) return <Spinner />;
+
+  const countries: ICountry[] = cities.reduce<ICountry[]>(
+    (arr, city) => {
+      if (!arr.some((el) => el.country === city.country)) {
+        return [
+          ...arr,
+          {
+            country: city.country,
+            emoji: city.emoji,
+            id: city.id, 
+          },
+        ];
+      }
+      return arr;
+    },
+    [] 
+  );
+
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking in a city on the map" />
+    );
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country: ICountry) => (
+        <CountryItem country={country} key={country.id} />
+      ))}
+    </ul>
+  );
+}
