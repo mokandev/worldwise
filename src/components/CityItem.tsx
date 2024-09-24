22af2910eb03fd96535391b6dddd163ccf0c1eb7
Carlos <carlos.flach15@gmail.com>
2024-09-24 17:05:45 -0300

    feat: adding the city item active class

--- src/components/CityItem.tsx
+++ src/components/CityItem.tsx
@@ -1,30 +1,37 @@
-import { Link } from 'react-router-dom';
-import { ICity } from '../App';
-import styles from './CityItem.module.css';
+import { Link } from "react-router-dom";
+import styles from "./CityItem.module.css";
+import { ICity, useCities } from "../contexts/CitiesContext";
 
 interface ICityItemProps {
-	city: ICity;
+  city: ICity;
 }
 
 const formatDate = (date: string) =>
-	new Intl.DateTimeFormat('en', {
-		day: 'numeric',
-		month: 'long',
-		year: 'numeric',
-	}).format(new Date(date));
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(new Date(date));
 
 export default function CityItem({ city }: ICityItemProps) {
-	const { cityName, emoji, date, id, position } = city;
+  const { currentCity } = useCities();
 
+  const { id: currentCityId } = currentCity as ICity;
+  const { cityName, emoji, date, id, position } = city;
 
-	return (
-		<li>
-			<Link to={`${id}?lat=${position.lat}&lng=${position.lng}`} className={styles.cityItem}>
-				<span className={styles.emoji}>{emoji}</span>
-				<h3 className={styles.name}>{cityName}</h3>
-				<time className={styles.date}>{formatDate(date)}</time>
-				<button className={styles.deleteBtn}>&times; </button>
-			</Link>
-		</li>
-	);
+  return (
+    <li>
+      <Link
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+        className={`${styles.cityItem}  ${
+          id === currentCityId ? styles["cityItem--active"] : ""
+        }`}
+      >
+        <span className={styles.emoji}>{emoji}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>{formatDate(date)}</time>
+        <button className={styles.deleteBtn}>&times; </button>
+      </Link>
+    </li>
+  );
 }
