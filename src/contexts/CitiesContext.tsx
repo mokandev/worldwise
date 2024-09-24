a59eeb54129bf0cdb5a81a4fbe5e021174bab06f
Carlos <carlos.flach15@gmail.com>
2024-09-24 17:04:17 -0300

    feat: format CitiesContext

--- src/contexts/CitiesContext.tsx
+++ src/contexts/CitiesContext.tsx
@@ -1,69 +1,95 @@
-import { createContext, FC, ReactNode, useContext, useEffect, useState } from "react";
+import {
+  createContext,
+  FC,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
 export interface ICity {
-  cityName: string
-  country: string
-  emoji: string
-  date: string
-  notes: string
+  cityName: string;
+  country: string;
+  emoji: string;
+  date: string;
+  notes: string;
   position: {
-    lat: number,
-    lng: number
-  },
-  id: number
+    lat: number;
+    lng: number;
+  };
+  id: number;
 }
 
 interface ICitiesContext {
-  cities: ICity[]
-  isLoading: boolean
+  cities: ICity[];
+  isLoading: boolean;
+  currentCity: ICity | {},
+  getCity: (id: string) => Promise<void>;
 }
 
 const defaultContextValue: ICitiesContext = {
   cities: [],
   isLoading: false,
+  currentCity: {},
+  getCity: async () => {},
 };
 
 const CitiesContext = createContext<ICitiesContext>(defaultContextValue);
 
-const BASE_URL = "http://localhost:8000"
+const BASE_URL = "http://localhost:8000";
 
 interface ICitiesProviderProps {
-  children: ReactNode
+  children: ReactNode;
 }
-
-
 
 const CitiesProvider: FC<ICitiesProviderProps> = ({ children }) => {
-    const [cities, setCities] = useState<ICity[]>([]);
-    const [isLoading, setIsLoading] = useState(false)
-  
-    async function fetchCities() {
-      try {
-        setIsLoading(true)
-        const res = await fetch(`${BASE_URL}/cities`)
-        const data = await res.json()
-        setCities(data)
-  
-      } catch {
-        alert("There was an error loading data..")
-      } finally {
-        setIsLoading(false)
-      }
-    }
-  
-    useEffect(() => {
-      fetchCities()
-    },[])
+  const [cities, setCities] = useState<ICity[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
 
-    return (
-        <CitiesContext.Provider value={{cities, isLoading}}>{children}</CitiesContext.Provider>
-    )
-}
+  async function fetchCities() {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities`);
+      const data = await res.json();
+      setCities(data);
+    } catch {
+      alert("There was an error loading data..");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchCities();
+  }, []);
+
+  const getCity = async (id: string) => {
+    setCurrentCity({})
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch {
+      alert("There was an error loading data..");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>
+      {children}
+    </CitiesContext.Provider>
+  );
+};
 
 const useCities = () => {
   const context = useContext(CitiesContext);
-  if(context === undefined) throw new Error("CitiesContext was used outside the CitiesProvider")
-  return context
-}
+  if (context === undefined)
+    throw new Error("CitiesContext was used outside the CitiesProvider");
+  return context;
+};
 
-export {CitiesProvider, useCities}
+export { CitiesProvider, useCities };
