2b58efef44ff7079bc09e2abb00f31afaf363439
Carlos Afonso <carlos.flach15@gmail.com>
2024-10-06 14:30:11 -0300

    feat: Displaying City Markers on the Map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,33 +1,31 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
-import styles from "./Map.module.css";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import { useState } from "react";
-import { LatLngTuple } from "leaflet";
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import styles from './Map.module.css';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { useState } from 'react';
+import { LatLngTuple } from 'leaflet';
+import { ICity, useCities } from '../contexts/CitiesContext';
 export default function Map() {
-  const navigate = useNavigate();
-  const [mapPosition, setMapPosition] = useState<LatLngTuple>([40, 0]);
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+	const navigate = useNavigate();
+	const [mapPosition, setMapPosition] = useState<LatLngTuple>([40, 0]);
+	const [searchParams, setSearchParams] = useSearchParams();
+	const { cities } = useCities();
 
-  return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <MapContainer
-        className={styles.map}
-        center={mapPosition}
-        zoom={13}
-        scrollWheelZoom={true}
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </div>
-  );
+	const lat = searchParams.get('lat');
+	const lng = searchParams.get('lng');
+
+	return (
+		<div className={styles.mapContainer} onClick={() => navigate('form')}>
+			<MapContainer className={styles.map} center={mapPosition} zoom={13} scrollWheelZoom={true}>
+				<TileLayer url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png' attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' />
+				{cities.map((city: ICity) => (
+					<Marker position={[city.position.lat, city.position.lng]} key={city.id}>
+					<Popup>
+						<span>{city.emoji}</span>
+						<span>{city.cityName}</span>
+					</Popup>
+				</Marker>
+				))}
+			</MapContainer>
+		</div>
+	);
 }
